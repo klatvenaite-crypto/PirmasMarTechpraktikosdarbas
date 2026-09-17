40d94c8da86636b44983ccabbdec458743bc8d66
Kotryna <klatvenaite@gmail.com>
2026-09-17 11:07:10 +0300

    mygtukas

--- app/src/main/java/com/example/martechpraktikosdarbas1/MainActivity.java
+++ app/src/main/java/com/example/martechpraktikosdarbas1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.martechpraktikosdarbas1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private boolean isPressed = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPressed == false) {
+                    btn.setTextColor(getResources().getColor(R.color.red));
+                    btn.setText("Pakeiskite teksto spalva");
+                    isPressed = true;
+                } else {
+                    btn.setTextColor(getResources().getColor(R.color.black));
+                    btn.setText("Pakeista teksto spalva");
+                    isPressed = false;
+                }
+            }
         });
     }
 }
